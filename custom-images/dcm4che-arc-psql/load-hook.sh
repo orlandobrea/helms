475b7d7a01bcfa519e8174a9a351f4bd5c82c389
Orlando Brea <orlando.brea@cloudx.com>
2020-05-16 13:19:50 -0300

    Prueba de alta de certificados en pacs

--- custom-images/dcm4che-arc-psql/load-hook.sh
+++ custom-images/dcm4che-arc-psql/load-hook.sh
@@ -8,6 +8,7 @@ echo "--DEBUG END--"
 set -e
 
 keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/cacerts.jks -noprompt -storepass secret
+keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/tls.crt -keystore /docker-entrypoint.d/configuration/keystores/key.jks -noprompt -storepass secret
 
 echo "--HOOK END--"
 
