9a83721cfefb8d4421517aae5625fb3bf91dc952
Orlando Brea <orlando.brea@parserdigital.com>
2020-06-09 08:38:49 -0300

    Elimino CSP para probar kibana

--- custom-images/dcm4che-arc-psql/build.sh
+++ custom-images/dcm4che-arc-psql/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --no-cache  -t orlandobrea/dcm4chee-arc-psql:5.22.1-secure .
-docker push orlandobrea/dcm4chee-arc-psql:5.22.1-secure
+docker build --no-cache  -t orlandobrea/dcm4chee-arc-psql:5.22.2-secure .
+docker push orlandobrea/dcm4chee-arc-psql:5.22.2-secure
