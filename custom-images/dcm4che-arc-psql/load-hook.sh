229d44b6093040441e06465c2dba2040e2bf853b
Orlando Brea <orlando.brea@parserdigital.com>
2020-06-19 09:02:19 -0300

    Cambio init de dcm4che-arc-psql local para que los certs sean opcionales

--- custom-images/dcm4che-arc-psql/load-hook.sh
+++ custom-images/dcm4che-arc-psql/load-hook.sh
@@ -9,10 +9,16 @@
 
 set -e
 
-echo "Adding certificate to cacerts.jks"
-keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/cacerts.jks -noprompt -storepass secret
-echo "Adding certificate to key.jks"
-keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/key.jks -noprompt -storepass secret
+echo "Initializing keystore..."
+FILE=/opt/certs/keycloak/ca.crt
+if [ -f "$FILE" ]; then
+    echo "Adding certificate to cacerts.jks"
+    keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/cacerts.jks -noprompt -storepass secret
+    echo "Adding certificate to key.jks"
+    keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/key.jks -noprompt -storepass secret    
+else
+    echo "No certs found (/opt/certs/keycloak/ca.crt not found)"
+fi
 
 echo "Ready to start app..."
 
