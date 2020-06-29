a1c32d72e71374d1ca489f7eb0021ece7b25a6a4
Orlando Brea <orlando.brea@parserdigital.com>
2020-06-29 09:09:54 -0300

    Corrijo ingress de gatekeeper

--- custom-images/dcm4che-arc-psql/load-hook.sh
+++ custom-images/dcm4che-arc-psql/load-hook.sh
@@ -6,18 +6,21 @@
 # echo "--DEBUG END--"
 
 
-
 set -e
 
 echo "Initializing keystore..."
 FILE=/opt/certs/keycloak/ca.crt
+
+# Debug
+cat $FILE | base64
+
 if [ -f "$FILE" ]; then
     echo "Adding certificate to cacerts.jks"
-    keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/cacerts.jks -noprompt -storepass secret
+    keytool -import -trustcacerts -alias keycloak -file $FILE -keystore /docker-entrypoint.d/configuration/keystores/cacerts.jks -noprompt -storepass secret
     echo "Adding certificate to key.jks"
-    keytool -import -trustcacerts -alias keycloak -file /opt/certs/keycloak/ca.crt -keystore /docker-entrypoint.d/configuration/keystores/key.jks -noprompt -storepass secret    
+    keytool -import -trustcacerts -alias keycloak -file $FILE -keystore /docker-entrypoint.d/configuration/keystores/key.jks -noprompt -storepass secret    
 else
-    echo "No certs found (/opt/certs/keycloak/ca.crt not found)"
+    echo "No certs found ($FILE not found)"
 fi
 
 
