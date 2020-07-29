cb8472b9efafeaf42aa7cedd4f0f0bbddf75b5cf
Orlando Brea <orlando.brea@parserdigital.com>
2020-07-28 21:29:45 -0300

    Actualizo version del custom dcm4che-keycloak a 10.0.1

--- custom-images/dcm4che-keycloak/build.sh
+++ custom-images/dcm4che-keycloak/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t orlandobrea/dcm4che-keycloak:9.0.2 .
-docker push orlandobrea/dcm4che-keycloak:9.0.2
+docker build -t orlandobrea/dcm4che-keycloak:10.0.1 .
+docker push orlandobrea/dcm4che-keycloak:10.0.1
